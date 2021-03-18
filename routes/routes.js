c74c6ed60099bddd45f60c3eb87a4a3db843bf15
Kermit2626 <oligray26@gmail.com>
2021-03-18 13:05:23 +0000

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,20 @@ router.get("/", (req, res) => {
     return res.status(200).json({ message: "connected" });
 })
 
+router.get("/home", (req, res) => {
+    return res.send("welcome to the home page");
+})
+
+router.get("/signup", (req, res) => {
+    return res.send("welcome to the signup page");
+})
+
+router.get("/login", (req, res) => {
+    return res.send("welcome to the login page");
+})
+
+router.get("/login/forgotten-password", (req, res) => {
+    return res.send("welcome to the forgot-password page");
+})
+
 module.exports = router;
