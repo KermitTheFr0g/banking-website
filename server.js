3d566a6b877d4a4606125df8ee3fd06b4198607a
Kermit2626 <oligray26@gmail.com>
2021-03-19 00:05:42 +0000

    server

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const port = process.env.port;
 const routes = require("./routes/routes");
+const login = require("./routes/login");
+
 
 //middleware
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes for different pages
-app.use("/", routes)
-
+app.use("/", routes);
+app.use("/api/user", login);
 
 app.listen(port, () => {
     console.log("server up and running");
