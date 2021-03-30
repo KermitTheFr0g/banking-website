d9c63214512c6eb7ca94b662e54488d909e219f8
Kermit2626 <oligray26@gmail.com>
2021-03-30 16:14:50 +0100

    login and signup validation completed

--- validation/user.js
+++ validation/user.js
@@ -1,10 +1,84 @@
+const urlencoded = require("body-parser/lib/types/urlencoded");
+const joi = require("joi");
+const { login } = require("../models/user");
+
+const loginSchema = joi.object({
+    username: joi.string()
+        .alphanum()
+        .min(4)
+        .max(30)
+        .required(),
+    password: joi.string()
+        .alphanum()
+        .min(11)
+        .max(30)
+        .required()
+})  
+
+const signupSchema = joi.object({
+    username: joi.string()
+        .alphanum()
+        .min(4)
+        .max(30)
+        .required(),
+
+    password: joi.string()
+        .alphanum()
+        .min(11)
+        .max(30)
+        .required(),
+    
+    email: joi.string()
+        .email()
+        .required(),
+
+    firstName: joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+    lastName: joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+    dob: joi.date()
+        .max("now")
+});
+
+
 var validation = {
     login: function(user){
-        return;
+        const loginValidation = loginSchema.validate({
+            username: user.username,
+            password: user.password
+        })
+
+        if(!loginValidation.error){
+            return;
+        } else {
+            return loginValidation.error;
+        }
     },
 
     signup: function(newUser){
-        return;
+        const signupValidation = signupSchema.validate({
+            username: newUser.username,
+            password: newUser.password,
+            email: newUser.email,
+            firstName: newUser.first_name,
+            lastName: newUser.last_name,
+            dob: newUser.dob
+        })
+        
+        if(!signupValidation.error){
+            return;
+        }else {
+            return signupValidation.error;
+        }
+
     }
 }
 
