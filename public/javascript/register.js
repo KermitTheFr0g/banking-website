1900d64b7f3c002dd7e9176b971ed8fb585c3a97
Kermit2626 <oligray26@gmail.com>
2021-04-02 19:08:59 +0100

    pog

---
+++ public/javascript/register.js
@@ -0,0 +1,36 @@
+async function login(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    var data = {
+        username: username,
+        password: password
+    }
+
+    console.log(data)
+    var url = "/api/user/login"
+
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    })
+    .then(res => res.text())
+    .then(text => {
+        console.log(text);
+        if(text == "user logged in"){
+            window.location.href = "/dashboard/user";
+        } else if(text == "logged in"){
+            window.location.href = "/admin/dashboard/user";
+        }
+
+        if(text == "Username or password Incorrect"){
+            document.getElementById("errorLabel").innerHTML = text;
+        }
+    })
+}
+
+
