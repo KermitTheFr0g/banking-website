e29f49977e91c27c01f5f53bb2ec464aba71fa23
Kermit2626 <oligray26@gmail.com>
2021-05-10 10:00:31 +0100

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -4,20 +4,30 @@ var admin = {
     getLoans: async function(){
         // getting all loans 
         // getting info from two tables loan and customer
-        const [result, schema] = await db.promise().query('SELECT SUM(loan_amount) AS total_amount, first_name, last_name, date_created FROM loan, customer');
-        
-        return result
+        const [result, schema] = await db.promise().query('SELECT loan_amount, first_name, last_name, date_created FROM loan, customer');
+        let totalLoanAmount = await db.promise().query('SELECT SUM(loan_amount) AS total FROM loan');
 
+        totalLoanAmount = totalLoanAmount[0][0].total;
 
+        /*
         var totalAmount = 0;
         var i;
         for(i = 0; i < result.length; i++){
             totalAmount += parseFloat(result[i].loan_amount);
         }
+        */
 
         return {
-            totalLoanAmount: totalAmount, 
+            totalLoanAmount: totalLoanAmount, 
             allLoans: result
+        }
+    },
+
+    getLoansTrial: async function(){
+        const [result, schema] = await db.promise().query('SELECT SUM(loan_amount) FROM loan');
+
+        return {
+            totalLoanAmount: result
         }
     }
     
